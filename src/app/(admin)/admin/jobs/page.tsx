501fc7b506ee6d5d9a79e97f384eb6c3ba61bf7d
Tacite243 <taciteiragi@gmail.com>
2025-12-12 13:26:55 +0200

    feat(build): update for production production

--- src/app/(admin)/admin/jobs/page.tsx
+++ src/app/(admin)/admin/jobs/page.tsx
@@ -1,6 +1,7 @@
 import React, { Suspense } from 'react';
 import JobOffersList from '@/components/JobOffersList';
 import JobOffersTableSkeleton from '@/components/JobOffersTableSkeleton';
+export const dynamic = "force-dynamic";
 
 
 
